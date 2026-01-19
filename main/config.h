1c32f9adb79aae4e7a7772f0e993f1406ff11750
PingVortex <nerfshelly123@gmail.com>
2026-01-19 18:28:53 +0100

    Small mistake

--- main/config.h
+++ main/config.h
@@ -3,8 +3,8 @@
 
  // --- SETTINGS ---
   const char* token = "URTOKEN"; // YOUR BOT TOKEN
-  String wifiName = "SSID"; // YOUR WIFI NAME/SSID
-  String wifiPassword = "PASSWORD"; // YOUR WIFI PASSWORD
-  String botName = "BOT"; // WHAT NAME DO YOU WANT TO BE DISPLAYED ON THE DISPLAY (Can be left as BOT)
+  const char* wifiName = "SSID"; // YOUR WIFI NAME/SSID
+  const char* wifiPassword = "PASSWORD"; // YOUR WIFI PASSWORD
+  const String botName = "BOT"; // WHAT NAME DO YOU WANT TO BE DISPLAYED ON THE DISPLAY (Can be left as BOT)
 
 #endif
